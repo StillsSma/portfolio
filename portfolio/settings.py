29dc417d7cf05fce479cb5ce7230163f35eb7938
StillsSma <samdawson301@live.com>
2016-12-30 15:22:32 -0500

    changed back secret key

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fdgdfdgfj554ygwefa3443s4waujdjh'
+SECRET_KEY = os.environ['SECRET_KEY_PORTFOLIO']
 print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
