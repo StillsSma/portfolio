0ac8d90e94acbce6af159cfd80de362c141f5137
StillsSma <samdawson301@live.com>
2016-12-30 13:30:12 -0500

    updated settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY_PORTFOLIO")
+SECRET_KEY = os.environ['SECRET_KEY_PORTFOLIO']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -85,7 +85,7 @@ import dj_database_url
 heroku_database = dj_database_url.config()
 if heroku_database:
     DATABASES["default"] = heroku_database
-    
+
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
