3459a848534ddc6dbb1f3067bdf2b68b6b019445
StillsSma <samdawson301@live.com>
2016-12-31 14:07:01 -0500

    can upload pictures

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,8 +2,10 @@
 from django.conf.urls import url
 from django.contrib import admin
 from app.views import IndexView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', IndexView.as_view(), name="index_view"),
-]
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
